3b8e67a583bb1cc845e2f4ac6b578d4fcb930bf1
minhnhut170701 <minhnhut170701@gmail.com>
2022-07-26 15:31:06 +0700

    deloy

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import {Box, Text} from '@chakra-ui/react'
 function App() {
 
   return (
-    <Box style={{backgroundColor: '#f6f7f9'}} position='relative'>
+    <Box style={{backgroundColor: '#f6f7f9'}} position='relative' w={{md: '100%', base: 'auto'}}>
         <Home />
         <Box position='fixed' w='90%' h='48px' p='2' display={{md: 'none', base: 'block'}} 
         bg="#339a99" bottom='0' left='30px' mb='20px' rounded='20px'>
--- src/components/HomePage/Footer/Footer.jsx
+++ src/components/HomePage/Footer/Footer.jsx
@@ -4,7 +4,8 @@ const Footer = () => {
   return (
     <Box w="100%" mt='40px' bg="#f8f8f8" borderTop='1px' borderColor='#e7e7e7' p='6'>
         <Box w={{md: '1223px', base: '100%'}} mx='auto'>
-            <Flex alignItems='center' textColor='#4E4E4E'>
+            <Flex alignItems='center' textColor='#4E4E4E' 
+            flexDirection={{md: 'row', base: 'column'}}>
                 <Image src="https://truyen.tangthuvien.vn/images/logo-web-gray.png"
                 alt='name logo' title='name logo' loading='lazy' maxW='100%' h='60px' />
                 <Box ml='40px'>
