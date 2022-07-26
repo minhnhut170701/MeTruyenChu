116686d203dfecc62a560cb1075d0fe19ac54f9e
minhnhut170701 <minhnhut170701@gmail.com>
2022-07-26 15:20:32 +0700

    edit responsive

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <Box style={{backgroundColor: '#f6f7f9'}} position='relative'>
         <Home />
-        <Box position='fixed' w='90%' h='48px' p='2' 
+        <Box position='fixed' w='90%' h='48px' p='2' display={{md: 'none', base: 'block'}} 
         bg="#339a99" bottom='0' left='30px' mb='20px' rounded='20px'>
           <Text textAlign='center' color='white'>Tải App TTV để sử dụng đầy đủ chức năng</Text>
         </Box>
